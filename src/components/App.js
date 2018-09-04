ec046fe32311cd30730c82297e73e67cfa75003d
Fulton Zink <joeyzink@gmail.com>
2018-09-04 16:10:05 -0700

    Add a small margin to the app.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import styles from '../styles/app.scss';
 import ImageSearch from './ImageSearch';
 
 const App = () => {
     return (
-            <div className={'App'}>
+            <div className={`App ${styles.app}`}>
                 <ImageSearch />
             </div>
     );
